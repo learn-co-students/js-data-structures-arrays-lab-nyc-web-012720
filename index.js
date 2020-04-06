2dd0d9bccc05337cf5b49a1ca904b015283c0e85
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-04-06 13:25:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-// Write your solution here!
-const drivers = ["Milo", "Otis", "Garfield"];
+const drivers = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendDriver(name) {
-    drivers.push(name);
+const destructivelyAppendDriver = (name) => {
+    drivers.push(name)
+};
+
+const destructivelyPrependDriver = (name) => {
+    drivers.unshift(name)
+};
+
+const destructivelyRemoveLastDriver = () => {
+    drivers.pop()
+};
+
+const destructivelyRemoveFirstDriver = () => {
+    drivers.shift()
+};
+
+const appendDriver = (name) => {
+    return drivers.concat(name)
+};
+
+const prependDriver = (name) => {
+   return [name, ...drivers]
 }
 
-function destructivelyPrependDriver(name) {
-    drivers.unshift(name);
+const removeLastDriver = () => {
+    return drivers.slice(0,drivers.length -1)
+};
+
+const removeFirstDriver = () => {
+    return drivers.slice(1)
 }
 
-function destructivelyRemoveLastDriver() {
-    drivers.pop();
-}
 
-function destructivelyRemoveFirstDriver() {
-    drivers.shift();
-}
-
-function appendDriver(name) {
-     return drivers.concat(name);
-}
-
-function prependDriver(name) {
-    return [name,...drivers];
-}
-
-function removeLastDriver(){
-    return drivers.slice(0, drivers.length -1);
-    //.slice takes in two arguments first argmuent 0 is index to begin in the array
-    // second argument is drivers.length -1 which removes the last driver.
-}
-
-function removeFirstDriver() {
-    return drivers.slice(1);
-    //returns the arrary starting from index 1 (removing driver at 0 index)
-}
 
 
